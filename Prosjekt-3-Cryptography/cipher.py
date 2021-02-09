f0550a2e3bbda15094e92e9388a82a69c69361b6
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2021-02-09 10:42:04 +0100

    Finished project

--- Prosjekt-3-Cryptography/cipher.py
+++ Prosjekt-3-Cryptography/cipher.py
@@ -1,6 +1,8 @@
 
-import crypto_utils
+import math
 import random
+import crypto_utils
+
 
 class Cipher:
 
@@ -10,6 +12,9 @@ class Cipher:
 
     def encode(self, message):
         return message
+
+    def set_key(self, key):
+        self.key = key
 
     def decode(self, message):
         return message
@@ -25,7 +30,6 @@ class Cipher:
 
 
 class Caesar(Cipher):
-
 
     def encode(self, message):
         new_message = ""
@@ -63,11 +67,11 @@ class Multiplication(Cipher):
             new_message += chr(new_value + 32)
         return new_message
 
+
 class Affine(Cipher):
 
-    def __init__(self, key1, key2):
-        super().__init__(key1)
-        self.key = [key1, key2]
+    def __init__(self, key_tuple):
+        super().__init__(key_tuple)
 
     def encode(self, message):
         new_message = ""
@@ -77,17 +81,16 @@ class Affine(Cipher):
             new_message += chr(new_value + 32)
         return new_message
 
-
-
-
     def decode(self, message):
         new_message = ""
-        modular_inverse = crypto_utils.modular_inverse(self.key[0], self.modulo)
+        modular_inverse = crypto_utils.modular_inverse(
+            self.key[0], self.modulo)
         for letter in message:
             value = ord(letter) - 32
             new_value = ((value - self.key[1]) * modular_inverse) % self.modulo
             new_message += chr(new_value + 32)
         return new_message
+
 
 class Unbreakable(Cipher):
 
@@ -98,9 +101,7 @@ class Unbreakable(Cipher):
             value2 = (ord(self.key[i % len(self.key)])) - 32
             new_value = (value1 + value2) % self.modulo
             new_message += chr(new_value + 32)
-        return  new_message
-
-
+        return new_message
 
     def decode(self, message):
         new_message = ""
@@ -109,49 +110,45 @@ class Unbreakable(Cipher):
             value2 = (ord(self.key[i % len(self.key)])) - 32
             new_value = (value1 - value2) % self.modulo
             new_message += chr(new_value + 32)
-        return  new_message
+        return new_message
 
 
 class RSA(Cipher):
 
     def __init__(self):
-        n, e, d = self.generate_keys()
-        self.public_key = (n, e)
-        self.private_key = (n, d)
-        print("n is %d" % n)
+        number, encrypt, decrypt = self.generate_keys()
+        self.public_key = (number, encrypt)
+        self.private_key = (number, decrypt)
 
     def generate_keys(self):
-        p = crypto_utils.generate_random_prime(8)
-        q = crypto_utils.generate_random_prime(8)
-        while p == q:
-            q = crypto_utils.generate_random_prime(8)
-        n = p*q
-        phi = (p-1)*(q-1)
-        e, d = 3, 3  # Setting initial values with gcd > 1
-        while crypto_utils.extended_gcd(e, phi)[0] != 1:
-            e = random.randint(3, phi - 1)
-            d = crypto_utils.modular_inverse(e, phi)
-        return n, e, d
-
+        prime1 = crypto_utils.generate_random_prime(8)
+        prime2 = crypto_utils.generate_random_prime(8)
+        while prime1 == prime2:
+            prime2 = crypto_utils.generate_random_prime(8)
+        number = prime2 * prime1
+        phi = (prime1 - 1) * (prime2 - 1)
+        encrypt, decrypt = 12, 16  # Setting initial values with gcd > 1
+        while math.gcd(encrypt, phi) != 1:
+            encrypt = random.randint(3, phi - 1)
+            decrypt = crypto_utils.modular_inverse(encrypt, phi)
+        return number, encrypt, decrypt
 
     def encode(self, message):
-        encryption_blocks = crypto_utils.blocks_from_text(message, 2)
-        print(f"encryption blocks: {encryption_blocks}")
+        encryption_blocks = crypto_utils.blocks_from_text(message, 1)
         new_blocks = []
         for block in encryption_blocks:
-            c = pow(block, self.public_key[1], self.public_key[0])
-            new_blocks.append(c)
-        print(f"new blocks: {new_blocks}")
+            encrypted_block = pow(block, self.public_key[1], self.public_key[0])
+            new_blocks.append(encrypted_block)
         return new_blocks
-
 
     def decode(self, message):
         new_blocks = []
         for block in message:
-            t = pow(block, self.private_key[1], self.private_key[0])
-            new_blocks.append(t)
+            decrypted_block = pow(block, self.private_key[1], self.private_key[0])
+            new_blocks.append(decrypted_block)
         new_message = crypto_utils.text_from_blocks(new_blocks, 8)
         return new_message
+
 
 def main():
     my_cipher = RSA()
--- Prosjekt-3-Cryptography/person.py
+++ Prosjekt-3-Cryptography/person.py
@@ -1,27 +1,176 @@
+import string
+import math
+import crypto_utils
+import cipher
+
+
+def read_from_file(filename):
+    with open(filename) as file:
+        contents = file.read()
+    return contents
 
 
 class Person:
 
-    def __init__(self):
-        pass
+    def __init__(self, key):
+        self.key = key
+        self.cipher = None
 
-    def set_key(self):
-        pass
+    def set_key(self, key):
+        self.key = key
+        self.cipher.set_key(key)
 
     def get_key(self):
-        pass
+        return self.key
 
-    def operate_cipher(self):
+    def set_cipher(self, cipher):
+        self.cipher = cipher
+        self.cipher.set_key(self.key)
+
+    def operate_cipher(self, message):
         pass
 
 
 class Sender(Person):
 
-    def set_key(self):
-        print("Setting key...")
+    def operate_cipher(self, message):
+        return self.cipher.encode(message)
 
 
 class Receiver(Person):
 
-    def set_key(self):
-        print("Setting key... hehe")
+    def set_cipher(self, cipher):
+        self.cipher = cipher
+
+    def operate_cipher(self, message):
+        self.cipher.set_key(self.key)
+        return self.cipher.decode(message)
+
+
+class Hacker(Person):
+
+    def __init__(self):
+        super().__init__(None)
+        self.type = None
+        self.possible_factors = [
+            i for i in range(
+                2, 95) if math.gcd(
+                i, 95) == 1]
+
+    def set_cipher(self, cipher):
+        self.cipher = cipher
+
+    def set_cipher_type(self, type):
+        """ This could be C, M, A, or U """
+        self.type = type
+
+    def count_words(self, message, word_list):
+        words_in_message = message.split(" ")  # Creating a list of words
+        for j in range(len(words_in_message)):
+            words_in_message[j] = words_in_message[j].strip(
+                string.punctuation).lower()
+        counter = 0
+        for word in words_in_message:  # Counting the occurence of words
+            if word in word_list:
+                counter += 1
+        return counter
+
+    def find_most_common(self, counting_dict):
+        highest_value = 0
+        most_common = ""
+        for key in counting_dict:  # Finding the message with the most words
+            if counting_dict[key] > highest_value:
+                highest_value = counting_dict[key]
+                most_common = key
+        return most_common
+
+    def operate_cipher(self, message):
+        if self.type in ['C', 'M', 'A', 'U']:
+            counting_dict = {}
+            word_list = read_from_file("english_words.txt").split(
+                "\n")  # Taking in the list of english words
+        if self.type == "C":
+            for i in range(95):  # Creating all different kinds of messages
+                new_message = ""
+                for letter in message:  # Shifting the value
+                    value = ord(letter) - 32
+                    new_value = (value - i) % self.cipher.modulo
+                    new_message += chr(new_value + 32)
+                counting_dict[new_message] = self.count_words(
+                    new_message, word_list)
+            return self.find_most_common(counting_dict)
+        elif self.type == "M":
+            for factor in self.possible_factors:
+                new_message = ""
+                modular_inverse = crypto_utils.modular_inverse(
+                    factor, self.cipher.modulo)
+                for letter in message:
+                    value = ord(letter) - 32
+                    new_value = (value * modular_inverse) % self.cipher.modulo
+                    new_message += chr(new_value + 32)
+                counting_dict[new_message] = self.count_words(
+                    new_message, word_list)
+            return self.find_most_common(counting_dict)
+        elif self.type == "A":
+            for factor in self.possible_factors:
+                for i in range(95):
+                    new_message = ""
+                    modular_inverse = crypto_utils.modular_inverse(
+                        factor, self.cipher.modulo)
+                    for letter in message:
+                        value = ord(letter) - 32
+                        new_value = (
+                            (value - i) * modular_inverse) % self.cipher.modulo
+                        new_message += chr(new_value + 32)
+                    counting_dict[new_message] = self.count_words(
+                        new_message, word_list)
+            return self.find_most_common(counting_dict)
+        elif self.type == "U":
+            counter = 0
+            for english_word in word_list[0:1000]:
+                new_message = ""
+                counter += 1
+                print(counter)
+                for i in range(len(message)):
+                    value1 = ord(message[i]) - 32
+                    value2 = ord(english_word[i % len(english_word)]) - 32
+                    new_value = (value1 - value2) % self.cipher.modulo
+                    new_message += chr(new_value + 32)
+                counting_dict[new_message] = self.count_words(
+                    new_message, word_list)
+            return self.find_most_common(counting_dict)
+        else:
+            return None
+
+
+def main():
+
+    # Creating the cipher
+    cipher1 = cipher.Unbreakable("above")
+
+    # Creating the people
+    person1 = Sender("above")
+    person2 = Receiver("above")
+    person1.set_cipher(cipher1)
+    person2.set_cipher(cipher1)
+
+    # Sending a message
+    sending_message = "hello you"
+    print("Sending the message: %s" % sending_message)
+    encoded_message = person1.operate_cipher(sending_message)
+    print("The encoded message is: %s" % encoded_message)
+
+    # Hacker trying to decode message
+    person3 = Hacker()
+    person3.set_cipher(cipher1)
+    person3.set_cipher_type("U")
+    hacked_message = person3.operate_cipher(encoded_message)
+    print("The hacked message is: %s" % hacked_message)
+
+    # Receiver decoding the message
+    decoded_message = person2.operate_cipher(encoded_message)
+    print("The decoded message is: %s" % decoded_message)
+
+
+if __name__ == "__main__":
+    main()
