66cdfd4817c77387c879586f3070e9e755b4d198
Jarl <jarl_sondre@hotmail.com>
2021-03-07 11:40:19 +0100

    Added kpc_agent skeleton and added comments to fsm

--- Prosjekt-5-Keypad/fsm.py
+++ Prosjekt-5-Keypad/fsm.py
@@ -1,24 +1,30 @@
 """ Class for the FSM """
 
+
 class FSM:
+    """ Class for the finite state machine """
 
     def __init__(self):
         self.state = 0
 
     def add_rule(self):
+        """ Add a new rule to end of the FSM's rule list """
         pass
-    
+
     def get_next_signal(self):
+        """ Query the agent for the next signal """
         pass
 
     def run(self):
+        """ Begin in the initial state and then
+        repeatedly call get_next_signal until reaching the final state """
         pass
 
     def match(self):
+        """ Check whether the rule condition is fulfillled """
         pass
 
     def fire(self):
+        """ use the consequent rule to set the next state of the FSM
+        and call the appropriate agent action method """
         pass
-
-
-    
---
+++ Prosjekt-5-Keypad/kpc_agent.py
@@ -0,0 +1,40 @@
+""" Class for the KPC Agent """
+
+
+class KPCAgent:
+    """ Class for the KPC Agent """
+
+    def __init__(self):
+        self.keypad = None
+
+    def reset_passcode_entry(self):
+        """ Clear passcode buffer and initiate a power up lighting sequence on the LED Board """
+        pass
+
+    def get_next_signal(self):
+        """ Query the next keypad for the next pressed key """
+        pass
+
+    def verify_login(self):
+        """ Check that the password entered matches password stored in file """
+        pass
+
+    def validate_passcode_change(self):
+        """ Check that new password is legal """
+        pass
+
+    def light_one_led(self):
+        """ Light the correct LED for the correct amount of time """
+        pass
+
+    def flash_leds(self):
+        """ Flash all LEDs on the LED Board """
+        pass
+
+    def twinkle_leds(self):
+        """ Twinkle all LEDs on the LED Board """
+        pass
+
+    def exit_action(self):
+        """ Call LED Board to initiate power down lighting sequence """
+        pass
