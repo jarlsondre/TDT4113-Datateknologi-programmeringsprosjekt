4a06d5e257a7de19dd9027cfd37f4476999525ae
Jarl <jarl_sondre@hotmail.com>
2021-03-07 11:21:24 +0100

    Added file for finite state machine

---
+++ Prosjekt-5-Keypad/fsm.py
@@ -0,0 +1,24 @@
+""" Class for the FSM """
+
+class FSM:
+
+    def __init__(self):
+        self.state = 0
+
+    def add_rule(self):
+        pass
+    
+    def get_next_signal(self):
+        pass
+
+    def run(self):
+        pass
+
+    def match(self):
+        pass
+
+    def fire(self):
+        pass
+
+
+    
