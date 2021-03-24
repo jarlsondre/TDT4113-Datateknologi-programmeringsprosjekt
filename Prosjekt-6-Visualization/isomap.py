9297846fcc749099af1b4ca2325a0ec3e764bf1d
Tomas Gule <vaagengule@hotmail.com>
2021-03-24 09:34:02 +0100

    Isomap

---
+++ Prosjekt-6-Visualization/isomap.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy as sy
+import matplotlib as plt
+from sklearn.utils.graph_shortest_path import graph_shortest_path
+
+
+class Isomap():
+    def __init__(self):
+        print("Hei")
+
+
