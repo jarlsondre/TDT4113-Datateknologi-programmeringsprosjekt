f938138f267617a77a9dfa9016c70bad330c4d1a
Tomas Gule <vaagengule@hotmail.com>
2021-03-24 19:50:35 +0100

    Funker litt?

--- Prosjekt-6-Visualization/isomap.py
+++ Prosjekt-6-Visualization/isomap.py
@@ -9,57 +9,63 @@ class Isomap():
         print("Hei")
 
 
+def isomap(X):
+    C = np.genfromtxt('digits_label.csv', delimiter=',')
+    C_sorted = np.argsort(C)
+
+    N, D = X.shape
+
+    X2 = np.square(X)
+
+    v = np.sum(X2, axis=1, keepdims=True)
+
+    D_EU2 = v + v.T - 2 * X @ X.T
+    D_EU = np.sqrt(np.abs(D_EU2))
+
+    if D == 64:
+        K = 30
+    elif D == 3:
+        K = 25
+
+    D_kNN = np.zeros((N, N))
+
+    for i, row in enumerate(D_EU):
+        ind = np.argpartition(row, K)[:K]
+        D_kNN[i][ind] = D_EU[i][ind]
+
+
+    D_geodesic = graph_shortest_path(D_kNN)
+
+    D_2 = np.square(D_geodesic)
+
+    I = np.identity(N)
+
+    ONE = np.ones(N)
+
+    J = I - ONE @ ONE.T / N / N
+    B = (-1 / 2) * J @ D_2 @ J
+    m = 2
+
+    if D - 1 == m:
+        eigenvalues, eigenvectors = np.linalg.eigh(B)
+        eigenvalues = eigenvalues[-m:]
+        eigenvectors = eigenvectors[:, -m:]
+
+    else:
+        eigenvalues, eigenvectors = eigs(B, k=m)
+
+    E_m = eigenvectors
+    LAMBDA = np.diag(eigenvalues)
+
+    Y = E_m @ np.sqrt(LAMBDA)
+    if D == 64:
+        plt.scatter(-Y[:, 0], Y[:, 1], c=C, s=10, marker='.')
+        plt.ylim(-100, 100)
+    if D == 3:
+        plt.scatter(Y[:, 1], Y[:, 0], c=np.arange(N), s=10, marker='.')
+        plt.ylim(-2, 2)
+    plt.show()
+
 X = np.genfromtxt('swiss_data.csv', delimiter=',')
-X2 = np.square(X)
-
-v = np.sum(X2, axis=1, keepdims=True)
-
-
-D_EU2 = v + v.T - 2 * X @ X.T
-D_EU = np.sqrt(np.absolute(D_EU2))
-
-
-K = 10
-D_kNN = D_EU
-for i, row in enumerate(D_kNN):
-    sorted_row = sorted(row)
-    threshold = sorted_row[K - 1]
-    for j, col in enumerate(row):
-        if col > threshold and i != j:
-            D_kNN[i][j] = 0
-
-D_geodesic = graph_shortest_path(D_kNN)
-
-D_2 = np.square(D_geodesic)
-
-N = len(D_2)
-I = np.identity(N)
-ONE = np.ones(N)
-
-J = I - 1/N * ONE @ ONE.transpose()
-
-B = -1/2 * J @ D_2 @ J
-m = 2
-eigenvalues, eigenvectors = np.linalg.eigh(B)
-eigenvalues = eigenvalues[-m:]
-for i in eigenvectors[-m:]:
-    print(i)
-
-# ind = np.argpartition(eigenvalues, -m)[-m:]
-#LAM = np.diag(eigenvalues[ind])
-#E_m = eigenvectors[:, ind]
-
-E_m = eigenvectors
-LAM = np.diag(eigenvalues)
-
-
-
-Y = E_m @ np.sqrt(LAM)
-
-
-plt.scatter(Y[:, 1], Y[:, 0], c=np.arange(N))
-plt.show()
-
-
-
-
+isomap(X)
+X = np.genfromtxt('digits.csv', delimiter=',') # K = 30
