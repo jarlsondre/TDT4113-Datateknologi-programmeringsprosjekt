b4da0ba3d0a9339f88202d24c48998e7530d8911
Jarl Sondre Bringslid Sæther <jssaethe@stud.ntnu.no>
2021-03-24 09:38:24 +0100

    added pca-file

---
+++ Prosjekt-6-Visualization/pca.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class PCA():
+
+    def __init__(self):
+        print("Made an object")
+    
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
+
+def main():
+    pca = PCA()
+
+if __name__ == "__main__":
+    main()
+
