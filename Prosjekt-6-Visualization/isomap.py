edcdd8c5979bf06df96a254b795dfdecd7ddc9bf
Tomas Gule <vaagengule@hotmail.com>
2021-03-24 21:22:31 +0100

    Deler en gang for mye på N, men ser bra ut

--- Prosjekt-6-Visualization/isomap.py
+++ Prosjekt-6-Visualization/isomap.py
@@ -16,10 +16,14 @@ def isomap(X):
     N, D = X.shape
 
     X2 = np.square(X)
+    print(X2)
 
     v = np.sum(X2, axis=1, keepdims=True)
 
+    print(v)
+
     D_EU2 = v + v.T - 2 * X @ X.T
+    print(D_EU2)
     D_EU = np.sqrt(np.abs(D_EU2))
 
     if D == 64:
@@ -30,27 +34,20 @@ def isomap(X):
     D_kNN = np.zeros((N, N))
 
     for i, row in enumerate(D_EU):
-        ind = np.argpartition(row, K)[:K]
+        ind = np.argpartition(row, K)[:K] # Index til de K minste elementene
         D_kNN[i][ind] = D_EU[i][ind]
 
-
     D_geodesic = graph_shortest_path(D_kNN)
-
     D_2 = np.square(D_geodesic)
-
     I = np.identity(N)
-
     ONE = np.ones(N)
-
     J = I - ONE @ ONE.T / (N * N)
     B = (-1 / 2) * J @ D_2 @ J
     m = 2
-
     if D - 1 == m:
         eigenvalues, eigenvectors = np.linalg.eigh(B)
         eigenvalues = eigenvalues[-m:]
         eigenvectors = eigenvectors[:, -m:]
-
     else:
         eigenvalues, eigenvectors = eigs(B, k=m)
 
@@ -59,14 +56,17 @@ def isomap(X):
 
     Y = E_m @ np.sqrt(LAMBDA)
     if D == 64:
-        plt.scatter(-Y[:, 0], Y[:, 1], c=C, s=10, marker='.')
+        plt.scatter(-Y[:, 0], Y[:, 1], c=C, s=10, marker='.', cmap='jet')
         plt.ylim(-100, 100)
     if D == 3:
-        plt.scatter(Y[:, 1], Y[:, 0], c=np.arange(N), s=10, marker='.')
+        plt.scatter(Y[:, 1], Y[:, 0], c=np.arange(N), s=10, marker='.', cmap='jet')
         plt.ylim(-1, 1)
     plt.show()
 
 X = np.genfromtxt('swiss_data.csv', delimiter=',')
 isomap(X)
+
 X = np.genfromtxt('digits.csv', delimiter=',') # K = 30
 isomap(X)
+
+
