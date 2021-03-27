dfa353bd6ccd24fbad03c6ecb352c58cda93f5e7
Bendik Holter <bendikholter@Bendiks-MacBook-Pro.local>
2021-03-27 10:48:11 +0100

    La til kommentarer

--- Prosjekt-6-Visualization/pca.py
+++ Prosjekt-6-Visualization/pca.py
@@ -1,19 +1,26 @@
+"""pca"""
 import numpy as np
 from scipy.sparse.linalg import eigs
 import matplotlib.pyplot as plt
 
 
 class PCA:
+    """
+    Klasse som bruker pca for å
+    konvertere datapunktene i swiss_data og digits
+    til 2D
+     """
 
     def __init__(self):
         self.X_matrix = None
         self.Y_matrix = None
 
     def fit(self):
+        """metode som beregner transformasjons-matrisen"""
         vectors_t = None
         sum_ = self.X_matrix.sum(axis=0)
         my = 1 / len(self.X_matrix) * sum_
-        self.X_matrix - my
+        self.X_matrix = self.X_matrix - my
         cov_matrix = np.cov(self.X_matrix.T)
         dim = cov_matrix.shape[0]
         if dim - 1 > 2:
@@ -25,23 +32,27 @@ class PCA:
         self.transform(vectors_t)
 
     def transform(self, transformation_matrix):
+        """Transformerer datapunktene til 2D"""
         lst = []
-        print(transformation_matrix.shape)
         for i in range(len(self.X_matrix)):
             tmp = np.matmul(transformation_matrix, self.X_matrix[i])
             lst.append(tmp)
         self.Y_matrix = np.array(lst)
 
     def show_result(self):
+        """Plotter resultatet av konverteringen"""
         if self.Y_matrix.shape[0] == 5620:
             C = np.genfromtxt("digits_label.csv", delimiter=',')
-            plt.scatter(self.Y_matrix[:, [0]], self.Y_matrix[:, [1]], s=10, c=C, cmap='jet', marker='.')
+            plt.scatter(self.Y_matrix[:, [0]],
+                        self.Y_matrix[:, [1]], s=10, c=C, cmap='jet', marker='.')
         else:
             C = np.arange(self.Y_matrix.shape[0])
-            plt.scatter(self.Y_matrix[:, [0]], self.Y_matrix[:, [1]], s=10,c =C, cmap='jet', marker='.')
+            plt.scatter(self.Y_matrix[:, [0]],
+                        self.Y_matrix[:, [1]], s=10,c =C, cmap='jet', marker='.')
         plt.show()
 
     def read_from_file(self, fil_navn):
+        """leser inn datapunktene fra fil"""
         self.X_matrix = np.genfromtxt(fil_navn, delimiter=',')
 
 if __name__ == "__main__":
